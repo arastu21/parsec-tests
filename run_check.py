97579792e23b037c10ec346a0c69a0ac132210bb
arastu21 <64304870+arastu21@users.noreply.github.com>
2020-06-09 01:45:45 +0530

    a

---
+++ run_check.py
@@ -0,0 +1,2 @@
+import gem5art.run
+for i in gem5art.run.getRunsByName(name='parsec classic memory tests with gem5-20', fs_only=True, limit=100):print(i)
---
+++ to_get_runs.py
@@ -0,0 +1,2 @@
+import gem5art.run
+for i in gem5art.run.getRuns(fs_only=False, limit=100):print(i)
